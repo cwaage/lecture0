d5dd80f079c7c09a5619674ac102f735057a7458
Chris Waage <cwaage@orcas.k12.wa.us>
2019-02-16 08:02:20 -0800

    Added Text Message

--- file_io.py
+++ file_io.py
@@ -4,15 +4,17 @@ import sys
 
 num_args = len(sys.argv)
 
-if num_args > 1:
+if num_args > 2:
 
 	file_name = sys.argv[1]
+	text = sys.argv[2]
 
 else:
 	
-	print("Usage: file_op.py <file_name>")
+	print("Usage: file_op.py <file_name> <message>")
 	sys.exit()
 
 outFile = open(file_name, "w")
+outFile.write(text)
 
 
